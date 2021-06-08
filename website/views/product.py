ac1bff5dada2b4801a5f6ff25ab5d65a597391a9
pritesh <pritesh.taline@gmail.com>
2021-06-08 18:56:03 +0530

    Product detail view
    - Django admin panel in models in add filter field and search field.
    - Product detail view in add product similar, recommended and vtov product section add
    - Product model in redirect_url filed charater length change.

--- website/views/product.py
+++ website/views/product.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView
 from products.models.product import Product
 from products.models.similar_product import SimilarProduct
+from products.models.recommended_product import RecommendedProduct
+from products.models.vtov_product import VtovProduct
 
 
 class ProductDeatilView(DetailView):
@@ -9,12 +11,21 @@ class ProductDeatilView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # pd = Product.objects.get(id=504).prefetch_related('similar_products')
-        # smp = SimilarProduct.objects.filter(product=504)
-        # for sm in smp:
-        #     print(sm.prefetch_related('similar_products'))
+        product_id = context['product'].id
 
-        # context['similar_products'] = smp
-        pd = SimilarProduct.objects.prefetch_related('product__similar_products')
-        print(pd)
+        similer_product_id_list = SimilarProduct.objects.values_list('similar_products', flat=True).filter(
+            product__id=product_id)
+        context['similer_products'] = Product.objects.filter(
+            id__in=similer_product_id_list)
+
+        recommended_product_id_list = RecommendedProduct.objects.values_list('recommended_products', flat=True).filter(
+            product__id=product_id)
+        context['recommended_products'] = Product.objects.filter(
+            id__in=recommended_product_id_list)
+
+        vtov_product_id_list = VtovProduct.objects.values_list('vtov_products', flat=True).filter(
+            product__id=product_id)
+        context['vtov_products'] = Product.objects.filter(
+            id__in=vtov_product_id_list)
+
         return context
