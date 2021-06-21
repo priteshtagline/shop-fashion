3d232737de1ecf830abcccb3cd5d7394e404192e
pritesh <pritesh.taline@gmail.com>
2021-06-21 11:47:27 +0530

    home page in display multipal campaign as per admin panel define order

--- website/views/home.py
+++ website/views/home.py
@@ -8,17 +8,6 @@ class HomeView(ListView):
     template_name = 'home.html'
     model = Campaign
 
-    def get_queryset(self):
-        """Override django generic listview get_queryset method because
-           as per requrments to diaplay campaigns product data for first campaigns products.
-
-        Returns:
-            [queryset object]: [filter first campaigns queryset object data]
-        """
-        qs = super(HomeView, self).get_queryset()
-
-        return qs[:1]
-
     def get_context_data(self, **kwargs):
         """Override django generic listview get_context_data method because
            as per requrments to diaplay campaigns product data with home carousel display.
