08d23aa9a606f135516bf0bc21813dd346a09b41
pritesh <pritesh.taline@gmail.com>
2021-06-25 16:33:46 +0530

    Improve the website response time

--- website/views/browse.py
+++ website/views/browse.py
@@ -1,10 +1,10 @@
+from django.db.models import Count, Q
+from django.db.models.functions import Lower
 from django.views.generic import ListView
 from products.models.category import Category
 from products.models.merchant import Merchant
 from products.models.product import Product
 from products.models.sub_category import SubCategory
-from collections import Counter
-from django.db.models import Count, Q
 
 
 class BrowseListView(ListView):
@@ -32,7 +32,6 @@ class BrowseListView(ListView):
         Returns:
             [queryset object]: [filter product queryset object data]
         """
-        qs = super(BrowseListView, self).get_queryset()
 
         kwargs_filters = {
             'department__name__iexact': self.kwargs['department'],
@@ -40,26 +39,25 @@ class BrowseListView(ListView):
 
         if 'category' in self.kwargs:
             kwargs_filters['category__name__iexact'] = self.kwargs['category']
-        
+
         if 'subcategory' in self.kwargs:
             kwargs_filters['subcategory__name__iexact'] = self.kwargs['subcategory']
-       
 
         filter_fields = ('brand', 'store', 'color')
-        query_dict = {}
+        query_dict = Q()
         for param in self.request.GET:
             if param in filter_fields and self.request.GET.get(param):
                 param_value_list = self.request.GET.get(param).split(',')
-                if param == 'brand':
-                    query_dict['brand__iregex'] = r'(' + \
-                        '|'.join(param_value_list) + ')'
-                elif param == 'store':
-                    query_dict['merchant__name__in'] = param_value_list
-                elif param == 'color':
-                    query_dict['color__iregex'] = r'(' + \
-                        '|'.join(param_value_list) + ')'
-
-        return qs.filter(**kwargs_filters, **query_dict)
+                for param_value in param_value_list:
+                    print(param_value)
+                    if param == 'brand':
+                        query_dict |= (Q(brand__iexact=param_value))
+                    elif param == 'store':
+                        query_dict |= Q(merchant__name__iexact=param_value)
+                    elif param == 'color':
+                        query_dict |= (Q(color__iexact=param_value))
+                        
+        return Product.objects.filter(**kwargs_filters).filter(query_dict)
 
     def get_context_data(self, **kwargs):
         """Override django generic listview get_context_data method because
@@ -72,6 +70,7 @@ class BrowseListView(ListView):
         Returns:
             [json object]: [product and his filter object data]
         """
+
         context = super().get_context_data(**kwargs)
 
         kwargs_filters = {
@@ -87,19 +86,18 @@ class BrowseListView(ListView):
         if 'subcategory' in self.kwargs:
             kwargs_filters['subcategory__name__iexact'] = self.kwargs['subcategory']
         else:
-            context['subcategories_list'] = SubCategory.objects.annotate(total_product=Count('product', distinct=True)).filter(**kwargs_filters).order_by('name')
-            
-        filter_brands_list = Product.objects.values_list(
-            'brand', flat=True).filter(**kwargs_filters).order_by('brand')
-        context['filter_brands'] = dict(
-            Counter(x.lower() for x in filter_brands_list))
+            context['subcategories_list'] = SubCategory.objects.annotate(total_product=Count(
+                'product', distinct=True)).filter(**kwargs_filters).order_by('name')
 
-        filter_color_list = Product.objects.values_list(
-            'color', flat=True).filter(**kwargs_filters).order_by('color')
-        context['filter_colors'] = dict(
-            Counter(x.lower() for x in list(filter(None, filter_color_list))))
-        
-        product_ids = [obj.id for obj in Product.objects.filter(**kwargs_filters)]
+        product_ids = [
+            product.id for product in Product.objects.filter(**kwargs_filters)]
+
+        context['filter_brands'] = Product.objects.annotate(brand_name=Lower('brand')).values('brand_name').filter(
+            id__in=product_ids).annotate(product_count=Count('brand_name')).order_by('brand_name')
+
+        context['filter_colors'] = Product.objects.annotate(color_name=Lower('color')).values('color_name').filter(
+            id__in=product_ids).annotate(product_count=Count('color_name')).filter(product_count__gt=1).exclude(color_name__exact='').order_by('color_name')
+
         context['filter_stors'] = Merchant.objects.annotate(total_product=Count(
             'product', filter=Q(product__id__in=product_ids))).filter(total_product__gt=1).order_by('name')
 
