54cc834ba83a4bdf948c5ebd89b690cf36cf22be
pritesh <pritesh.taline@gmail.com>
2021-07-05 10:30:51 +0530

    Each html page custom js separate file create and load

---
+++ static/js/base.js
@@ -0,0 +1,63 @@
+ $(function () {
+     $('.lazy').Lazy({
+         effect: 'show',
+     });
+ });
+
+ $(document).ready(function () {
+     $(document).on('submit', 'form.email-subscribe-form', function (e) {
+         form = $(this);
+         e.preventDefault();
+         $.ajax({
+             url: email_subscribe_url,
+             method: 'POST',
+             data: form.serialize(),
+             dataType: 'JSON',
+             headers: {
+                 "X-CSRFToken": form.find("[name=csrfmiddlewaretoken]").val()
+             },
+             success: function (data) {
+                 if (data.status) {
+                     form.find("p").empty().show().html(data.message).delay(1500).fadeOut(300);
+                     setTimeout(function () {
+                         form.find("input[type=email]").val("");
+                     }, 1500);
+                 }
+             },
+             error: function (data) {
+                 console.log(data);
+             },
+         });
+     });
+ });
+
+ $(document).on('click', '.like-product', function (e) {
+     e.preventDefault();
+     selector_heart = $(this).find('i')
+     like_action = $.inArray('far', selector_heart.attr('class').split(' ')) != -1 ? 'add' : 'remove'
+     $.ajax({
+         url: like_product_url,
+         method: 'POST',
+         data: {
+             'product_id': $(this).attr('value'),
+             'like_action': like_action
+         },
+         dataType: 'JSON',
+         headers: {
+             "X-CSRFToken": $("[name=csrfmiddlewaretoken]").val()
+         },
+         success: function (data) {
+             if (data.status) {
+                 selector_heart.toggleClass('fa');
+                 selector_heart.toggleClass('far');
+             } else {
+                 if (data.message == 'login_required') {
+                     window.location = "{% url 'user:login' %}?next={{request.path}}";
+                 }
+             }
+         },
+         error: function (data) {
+             console.log(data);
+         },
+     });
+ });
